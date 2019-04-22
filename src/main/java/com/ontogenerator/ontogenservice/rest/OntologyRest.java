34b188db001e2b1e1d042b9884f2bd1a84a5c980
lankeshw2 <lankeshw2@gmail.com>
2019-04-22 17:21:29 +1200

    removed imports

--- src/main/java/com/ontogenerator/ontogenservice/rest/OntologyRest.java
+++ src/main/java/com/ontogenerator/ontogenservice/rest/OntologyRest.java
@@ -5,29 +5,8 @@
  */
 package com.ontogenerator.ontogenservice.rest;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import java.io.File;
-import java.io.Reader;
-import java.util.List;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import org.geneontology.obographs.io.OgJsonGenerator;
-import org.geneontology.obographs.model.Graph;
-import org.geneontology.obographs.owlapi.FromOwl;
-import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLClass;
-import org.semanticweb.owlapi.model.OWLDataProperty;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
